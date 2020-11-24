fecf9ee9920db58c76843277e991a03ea6b04769
Vadym Makevytss <vadymlat@mail.ru>
2020-11-24 12:22:21 +0200

    Updating post with image added

--- backend/models/post.js
+++ backend/models/post.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 
+// @ts-ignore
 const postSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  imagePath: { type: String, required: true }
+  imagePath: { type: String, required: true },
 });
 
 module.exports = mongoose.model("Post", postSchema);
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,13 +1,14 @@
-import { Component, OnInit, OnDestroy } from "@angular/core";
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PageEvent } from '@angular/material';
 import { Subscription } from 'rxjs';
 
-import { Post } from "../post.model";
-import { PostsService } from "../posts.service";
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
-  selector: "app-post-list",
-  templateUrl: "./post-list.component.html",
-  styleUrls: ["./post-list.component.css"]
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent implements OnInit, OnDestroy {
   // posts = [
@@ -17,9 +18,12 @@ export class PostListComponent implements OnInit, OnDestroy {
   // ];
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 10;
+  postsPerPage = 5;
+  pageSizeOpt = [1, 2, 5, 10];
   private postsSub: Subscription;
 
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService) { }
 
   ngOnInit() {
     this.isLoading = true;
@@ -33,6 +37,10 @@ export class PostListComponent implements OnInit, OnDestroy {
 
   onDelete(postId: string) {
     this.postsService.deletePost(postId);
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    console.log(pageData);
   }
 
   ngOnDestroy() {
